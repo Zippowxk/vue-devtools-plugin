4111e4a8be9b4536ad490d23d2528b8a4c4cad23
王鑫凯 <wangxinkai@haier.com>
2021-03-12 16:00:31 +0800

    chore: remove useless code

--- packages/index.js
+++ packages/index.js
@@ -4,11 +4,9 @@ import {installHooks} from 'app-backend'
 import injectString from './inject.txt'
 installHooks(window)
 let file = `<div id='app'>GeeksForGeeks</div>`
-// let file = `<p>hello world</p>`
 let target;
 let targetWindow;
 const injectOnce = once(inject);
-let app='';
 class VConsoleVueTab extends VConsolePlugin {
 
   constructor(...args) {
@@ -16,26 +14,12 @@ class VConsoleVueTab extends VConsolePlugin {
   }
 
   onRenderTab(cb){
-    // cb('<iframe id="vue-iframe" src="./devtools.html" style="width:100%;position:absolute;top:0;bottom:0;min-height:100%;"></iframe>');
-    // cb('<div id="app">hello</div>');
-    // cb('<iframe id="vue-iframe" style="width:100%;position:absolute;top:0;bottom:0;min-height:100%;"></iframe>');
     cb(`<iframe id="vue-iframe" srcdoc="${file}" style="width:100%;position:absolute;top:0;bottom:0;min-height:100%;"></iframe>`);
   }
   onReady() {
     target = document.getElementById('vue-iframe')
     targetWindow = target.contentWindow;
-    be.initBackendWithTargetWindow(window,targetWindow);
-    // do nothing
-    // 引入 Vue
-    // myplugin().then((res)=>{
-    //   res.init();
-    // })
-    // mytest().then((res)=>{
-    //   console.log('====================================');
-    //   console.log(res);
-    //   console.log('load test js done');
-    // })
-    
+    be.initBackendWithTargetWindow(window,targetWindow);    
   }
 
   onAddTopBar(callback) {
@@ -63,10 +47,7 @@ function once(fn){
 function inject (scriptContent, done) {
   const script = document.getElementById('vue-iframe').contentWindow.document.createElement('script')
   script.text = scriptContent
-  // script.src = scriptContent;
-  // script.onload = done
   document.getElementById('vue-iframe').contentWindow.document.body.appendChild(script)
-  // console.log("injected!!!!!!!!!!!!!!!!!!");
 }
 
 const initPlugin = function(vConsole){

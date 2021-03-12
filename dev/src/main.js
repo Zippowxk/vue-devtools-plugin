b60a511c1c1ec16734b20a33eeef2a53f6306f20
王鑫凯 <wangxinkai@haier.com>
2021-03-12 12:58:09 +0800

    v0.0.2

--- dev/src/main.js
+++ dev/src/main.js
@@ -1,12 +1,12 @@
 import VConsole from "vconsole";
-// import vvp from 'vue-vconsole-plugin'
+import vvp from 'vue-vconsole-devtools'
 // import vvp from "./vue_plugin.min.js";
-import { initPlugin } from "./vue_plugin.esm.min.js";
+// import { initPlugin } from "./vue_plugin.esm.min.js";
 import Vue from "vue";
 import App from "./App.vue";
 new VConsole();
 // const vc = new VConsole();
-console.log(initPlugin);
+console.log(vvp);
 // vvp.initPlugin(vc);
 Vue.config.productionTip = false;
 new Vue({

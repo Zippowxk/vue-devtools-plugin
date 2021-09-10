d056075fa02c68e502be388222eff2c1491f98d3
王鑫凯 <wangxinkai@haier.com>
2021-09-10 15:13:10 +0800

    chore: dev project test cdn

--- dev/src/main.js
+++ dev/src/main.js
@@ -8,16 +8,16 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-if(process.env.NODE_ENV === "development"){
-  Promise.all([import("vconsole"), import("./debug/vue_plugin.js")]).then(
-    (res) => {
-      if (res.length === 2) {
-        Vue.config.devtools = true;
-        window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit("init",Vue)
-        const VConsole = res[0].default;
-        const Devtools = res[1].default;
-        Devtools.initPlugin(new VConsole());
-      }
-    }
-  );
-}
+// if(process.env.NODE_ENV === "development"){
+//   Promise.all([import("vconsole"), import("./debug/vue_plugin.js")]).then(
+//     (res) => {
+//       if (res.length === 2) {
+//         Vue.config.devtools = true;
+//         window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit("init",Vue)
+//         const VConsole = res[0].default;
+//         const Devtools = res[1].default;
+//         Devtools.initPlugin(new VConsole());
+//       }
+//     }
+//   );
+// }

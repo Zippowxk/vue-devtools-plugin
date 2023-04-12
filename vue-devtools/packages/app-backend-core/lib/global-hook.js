cd90a480790455b8eddb8d99402fec0c3d964bdc
王鑫凯 <wangxinkai@haier.com>
2023-04-12 14:40:27 +0800

    important: 修复hook找不到的问题,使用get解决,只能在打包后修复,esm不支持此修复方式

--- vue-devtools/packages/app-backend-core/lib/global-hook.js
+++ vue-devtools/packages/app-backend-core/lib/global-hook.js
@@ -1,14 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.hook = void 0;
-Object.defineProperty(exports, "hook", { value: void 0, configurable: true});
 const shared_utils_1 = require("@vue-devtools/shared-utils");
-// hook should have been injected before this executes.
-Object.defineProperty(exports, "hook", { get(){
+Object.defineProperty(exports, "hook", { get: function() {
   return shared_utils_1.target.__VUE_DEVTOOLS_GLOBAL_HOOK__
-},
-  configurable: true
-,});
-
-// exports.hook = shared_utils_1.target.__VUE_DEVTOOLS_GLOBAL_HOOK__;
+} });
 //# sourceMappingURL=global-hook.js.map
